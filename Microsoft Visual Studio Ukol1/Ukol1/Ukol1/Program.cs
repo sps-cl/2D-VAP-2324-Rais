304d884c0367ea81436fda4157d0bbe0ce0855d4
vojtechrais <vojtik203@seznam.cz>
2024-04-02 16:54:19 +0200

    asi to mám

---
+++ Microsoft Visual Studio Ukol1/Ukol1/Ukol1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Ukol1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Zadej sekvenci čisel");
+                string text = Console.ReadLine();
+                string[] hodnoty = text.Split(',');
+                int[] cisla = new int[hodnoty.Length];
+                bool chyba = false;
+                for (int i = 0; i < cisla.Length; i++)
+                {
+                    if (!int.TryParse(hodnoty[i], out cisla[i]))
+                    {
+                        chyba = true;
+                        break;
+                    };
+                }
+                if (chyba)
+                {
+                    Console.WriteLine("Spatny vstup.");
+                    Console.ReadLine();
+                    continue;
+                }
+                int min = cisla[0];
+                for (int i = 1; i < cisla.Length; i++)
+                {
+                    if (cisla[i] < min)
+                    {
+                        min = cisla[i];
+                    }
+                }
+
+                int max = cisla[0];
+                for (int i = 1; i < cisla.Length; i++)
+                {
+                    if (cisla[i] > max)
+                    {
+                        max = cisla[i];
+                    }
+                }
+
+                int maxLength = 1;
+                int currentLength = 1;
+                for (int i = 1; i < cisla.Length; i++)
+                {
+                    if (cisla[i] > cisla[i - 1])
+                    {
+                        currentLength++;
+                        if (currentLength > maxLength)
+                            maxLength = currentLength;
+                    }
+                    else
+                    {
+                        currentLength = 1;
+                    }
+                }
+
+                Console.WriteLine($"Nejmenší číslo je {min}.");
+                Console.WriteLine($"Největší číslo je {max}.");
+                Console.WriteLine($"Délka nejdelšího vzestupného intervalu je {maxLength}.");
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
